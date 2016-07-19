3eebdb905fe3ae7776a1bf0d25cfead934bd27eb
Afaren <550047450@qq.com>
2016-07-19 11:34:12 +0800

    add test of functions

--- pos_v0.1/test/main-test.js
+++ pos_v0.1/test/main-test.js
@@ -1,7 +1,7 @@
-describe('pos', function() {
+describe('pos', function () {
   var inputs;
 
-  beforeEach(function() {
+  beforeEach(function () {
     inputs = [
       {
         barcode: 'ITEM000000',
@@ -55,7 +55,76 @@ describe('pos', function() {
     ];
   });
 
-  it('should print correct text', function() {
+  it('should calculate "count" property and add it into each item', function () {
+    let expectedCountedCartItems = [
+      {
+        barcode: 'ITEM000000',
+        name: '可口可乐',
+        unit: '瓶',
+        price: 3.00,
+        count: 5
+      },
+      {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00,
+        count: 2
+      },
+      {
+        barcode: 'ITEM000004',
+        name: '电池',
+        unit: '个',
+        price: 2.00,
+        count: 1
+      }
+    ]
+    let acutal = itemCount(inputs);
+    expect(acutal).toEqual(expectedCountedCartItems);
+  });
+
+  it('should calculate "subTotal" property and add it into each item', function () {
+    let expectedSubTotalCountedCartedItems = [
+
+      {
+        barcode: 'ITEM000000',
+        name: '可口可乐',
+        unit: '瓶',
+        price: 3.00,
+        count: 5,
+        subTotal: 15
+      },
+      {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00,
+        count: 2,
+        subTotal: 6
+      },
+      {
+        barcode: 'ITEM000004',
+        name: '电池',
+        unit: '个',
+        price: 2.00,
+        count: 1,
+        subTotal: 2
+
+      }
+    ];
+
+    let actual = calculateSubTotal(itemCount(inputs));
+    expect(actual).toEqual(expectedSubTotalCountedCartedItems);
+
+  });
+  it('should calculate "totalPrice" of receipt', function () {
+    let expectedTotalPrice = 23;
+    let actual = calculateTotalPrice(calculateSubTotal(itemCount(inputs)));
+    expect(actual).toEqual(expectedTotalPrice);
+
+  })
+
+  it('should print correct text', function () {
 
     spyOn(console, 'log');
 
@@ -72,4 +141,5 @@ describe('pos', function() {
 
     expect(console.log).toHaveBeenCalledWith(expectText);
   });
-});
+})
+;
