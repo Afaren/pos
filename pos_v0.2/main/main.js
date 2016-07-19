27d410950881705e5241f237c1fbfc4b7e0cd0d8
Afaren <550047450@qq.com>
2016-07-19 23:09:39 +0800

    complete v0.2 test & implement

--- pos_v0.2/main/main.js
+++ pos_v0.2/main/main.js
@@ -1,4 +1,116 @@
-//TODO: Please write code in this file.
-function printReceipt(inputs) {
+// function getCartItems(allItems, barcodes) {
+//   let cartItems = [];
+//
+//   for (let barcode of barcodes) {
+//     let foundItem = allItems.find((item)=> {
+//       return Object.is(item.barcode, barcode);
+//     });
+//     cartItems.push(Object.assign({}, foundItem));
+//   }
+//   return cartItems;
+// }
 
+
+function calculateBarcodeCount(barcodes) {
+  let barcodeCounts = [];
+  for (let barcode of barcodes) {
+    // console.log(barcodes);
+    let barcodeAlreadyInside = barcodeCounts.find((toBeFound)=> {
+      // console.log('afar   '+ toBeFound);
+      return Object.is(toBeFound.barcode, barcode);
+    });
+    if (barcodeAlreadyInside) {
+      barcodeAlreadyInside.count++;
+    } else {
+      barcodeCounts.push(Object.assign({}, {barcode: barcode}, {count: 1}));//这里纠结了很久
+    }
+  }
+
+  return barcodeCounts;
+}
+
+function calculateItemCount(barcodeCounts, allItems) {
+  let countedCartItems = [];
+
+  for (let foo of barcodeCounts) {
+    let existItem = allItems.find((item)=> {
+      return Object.is(item.barcode, foo.barcode);
+    });
+    if (existItem) {
+      // existItem.count = foo.count; // 就是这里写错了，欸
+      countedCartItems.push(Object.assign({}, existItem, {count: foo.count}));
+    }
+  }
+
+  return countedCartItems;
+}
+
+function calculateSubTotal(countedCartItems) {
+
+  let subCountedCartItems = [];
+  for (let item of countedCartItems) {
+    subCountedCartItems.push(Object.assign({}, item, {subTotal: item.price * item.count}));
+  }
+
+
+  return subCountedCartItems;
+
+}
+
+function calculateTotalPrice(subTotalCartItems) {
+
+  let totalPrice = 0;
+  for (let item of subTotalCartItems) {
+    totalPrice += item.subTotal;
+  }
+  return totalPrice;
+}
+function generateHeader() {
+  return '***<没钱赚商店>收据***\n';
+
+}
+function generateBody(subTotalCartItems) {
+  let body = '';
+
+  for (let item of subTotalCartItems) {
+    let itemReceipt = '名称：' + item.name +
+      '，数量：' + item.count + item.unit +
+      '，单价：' + item.price.toFixed(2) +
+      '(元)，小计：' + item.subTotal.toFixed(2) + '(元)\n';
+    body = body.concat(itemReceipt);
+  }
+  return body;
+
+}
+function generateFooter(totalPrice) {
+  return '----------------------\n' +
+    '总计：' + totalPrice.toFixed(2) + '(元)\n' +
+    '**********************';
+
+}
+function generateReceipt(totalPrice, subTotalCartItems) {
+
+  let receipt = '';
+
+  let header = generateHeader();
+  let body = generateBody(subTotalCartItems);
+  let footer = generateFooter(totalPrice);
+
+  receipt = receipt.concat(header).concat(body).concat(footer);
+
+  return receipt;
+
+}
+
+// implement
+function printReceipt(inputs) {
+  let allItems = loadAllItems();
+  let barcodeCounts = calculateBarcodeCount(inputs);
+  let countedCartItems = calculateItemCount(barcodeCounts, allItems);
+  let subTotalCartItems = calculateSubTotal(countedCartItems);
+  let totalPrice = calculateTotalPrice(subTotalCartItems);
+  //noinspection UnnecessaryLocalVariableJS
+  let receipt = generateReceipt(totalPrice, subTotalCartItems);
+
+  console.log(receipt);
 }
--- pos_v0.2/test/main-test.js
+++ pos_v0.2/test/main-test.js
@@ -1,8 +1,8 @@
-describe('pos', function() {
+describe('pos', function () {
   var allItems;
   var inputs;
 
-  beforeEach(function() {
+  beforeEach(function () {
     allItems = loadAllItems();
     inputs = [
       'ITEM000000',
@@ -16,7 +16,162 @@ describe('pos', function() {
     ];
   });
 
-  it('should print correct text', function() {
+  // it('should calculate "count" of barcode when ')
+  it('should calculate "count" of barcode', function () {
+    let expectedBarcodeCounts = [
+      {
+        barcode: 'ITEM000000',
+        count: 5
+      },
+      {
+        barcode: 'ITEM000001',
+        count: 2
+      },
+      {
+        barcode: 'ITEM000004',
+        count: 1
+      }
+    ];
+    let actual = calculateBarcodeCount(inputs);
+    expect(actual).toEqual(expectedBarcodeCounts);
+  });
+
+  it('should calculate countedCartItems', function () {
+    let expected = [
+      {
+        barcode: 'ITEM000000',
+        name: '可口可乐',
+        unit: '瓶',
+        price: 3.00,
+        count: 5
+      },
+      {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00,
+        count: 2
+      },
+      {
+        barcode: 'ITEM000004',
+        name: '电池',
+        unit: '个',
+        price: 2.00,
+        count: 1
+      }
+    ];
+    let barcodeCounts = [
+      {
+        barcode: 'ITEM000000',
+        count: 5
+      },
+      {
+        barcode: 'ITEM000001',
+        count: 2
+      },
+      {
+        barcode: 'ITEM000004',
+        count: 1
+      }
+    ];
+    // let actual = calculateItemCount(barcodeCounts, loadAllItems());
+    let actual = calculateItemCount(barcodeCounts, loadAllItems());
+    expect(actual).toEqual(expected);
+  });
+
+  it('should calculate "subTotal" of items', function () {
+    let expectedCartItems = [
+      {
+        barcode: 'ITEM000000',
+        name: '可口可乐',
+        unit: '瓶',
+        price: 3.00,
+        count: 5,
+        subTotal: 15
+      },
+      {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00,
+        count: 2,
+        subTotal: 6
+      },
+      {
+        barcode: 'ITEM000004',
+        name: '电池',
+        unit: '个',
+        price: 2.00,
+        count: 1,
+        subTotal: 2
+
+      }
+    ];
+    let countedCartItems = [
+      {
+        barcode: 'ITEM000000',
+        name: '可口可乐',
+        unit: '瓶',
+        price: 3.00,
+        count: 5,
+      },
+      {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00,
+        count: 2,
+      },
+      {
+        barcode: 'ITEM000004',
+        name: '电池',
+        unit: '个',
+        price: 2.00,
+        count: 1,
+      }
+    ];
+    let actual = calculateSubTotal(countedCartItems);
+    expect(actual).toEqual(expectedCartItems);
+
+  });
+
+  it('should calculate "totalPrice" of receipt', function () {
+    let expectedTotalPrice = 23;
+    let subTotalCartItems = [
+      {
+        barcode: 'ITEM000000',
+        name: '可口可乐',
+        unit: '瓶',
+        price: 3.00,
+        count: 5,
+        subTotal: 15
+      },
+      {
+        barcode: 'ITEM000001',
+        name: '雪碧',
+        unit: '瓶',
+        price: 3.00,
+        count: 2,
+        subTotal: 6
+      },
+      {
+        barcode: 'ITEM000004',
+        name: '电池',
+        unit: '个',
+        price: 2.00,
+        count: 1,
+        subTotal: 2
+
+      }
+
+
+    ]
+    let actual = calculateTotalPrice(subTotalCartItems);
+    expect(actual).toEqual(expectedTotalPrice);
+  });
+
+
+  it('should print correct text', function () {
 
     spyOn(console, 'log');
 
